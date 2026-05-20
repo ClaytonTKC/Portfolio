62735d526ee93cc08a2421c068f7f7880e06423b
ClaytonTKC <Claytoncheung31@gmail.com>
2026-05-20 12:16:17 -0400

    test

--- frontend/src/components/sections/Education.tsx
+++ frontend/src/components/sections/Education.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card } from '../ui/Card';
 import { contentService, type Education as EducationType } from '../../services/content.service';
+//
 
 export const Education: React.FC = () => {
     const { t, i18n } = useTranslation();
